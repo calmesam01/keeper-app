4b2b96ece9d414119f4fd1daf5cde6aa8b38a788
calmesam01 <sanyamsareen01@gmail.com>
2020-04-02 21:56:12 -0400

    Last edit for Netlify

---
+++ build/precache-manifest.72f6987e8c48f6a7d95eb6697263b90b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1754639fe4b22bd3297081bc3ae0d223",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f84989c00001be03b2",
+    "url": "/static/js/2.cf582562.chunk.js"
+  },
+  {
+    "revision": "08ac12ad59ea6e36c861",
+    "url": "/static/js/main.0ccd4a9f.chunk.js"
+  },
+  {
+    "revision": "72b8f23938aa1bf9b600",
+    "url": "/static/js/runtime-main.7f6d9f94.js"
+  }
+]);
